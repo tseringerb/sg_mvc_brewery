c98d836332f19c5fe42175429a3541f0b9d0db3f
taskurim <borystserinher@gmail.com>
2021-08-29 22:28:35 +0300

    created project

---
+++ src/main/java/springgurucourse/sg_mvc_brewery/SgMvcBreweryApplication.java
@@ -0,0 +1,13 @@
+package springgurucourse.sg_mvc_brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgMvcBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SgMvcBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/springgurucourse/sg_mvc_brewery/SgMvcBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package springgurucourse.sg_mvc_brewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SgMvcBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

d4bb82110d514c206ebcdd7e0592cea8bd379ebf
taskurim <borystserinher@gmail.com>
2021-08-30 16:37:30 +0300

    created model for beer entity and controller for beer entity

---
+++ src/main/java/springgurucourse/sg_mvc_brewery/web/controller/BeerController.java
@@ -0,0 +1,21 @@
+package springgurucourse.sg_mvc_brewery.web.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import springgurucourse.sg_mvc_brewery.web.model.BeerDTO;
+
+import java.util.UUID;
+
+@RequestMapping("/api/v1/beer")
+@RestController
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDTO> getBeerById(@PathVariable("beerId") UUID beerId){
+        return new ResponseEntity<>(BeerDTO.builder().build(), HttpStatus.OK);
+    }
+}
